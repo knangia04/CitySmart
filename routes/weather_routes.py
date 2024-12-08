e37fbe6fb0df5ba313827f8c26bddc9dc0c67ce4
cookie-kyu <jingtongwang149@gmail.com>
2024-12-08 12:50:39 -0600

    no more SQL alchemy <3

--- routes/weather_routes.py
+++ routes/weather_routes.py
@@ -1,27 +1,42 @@
-from flask import Blueprint, render_template
-from sqlalchemy.sql import text
-from models import db
+import sqlite3
 from flask import Blueprint, render_template, request
+
 weather_bp = Blueprint('weather', __name__)
+
+def get_db_connection():
+    """
+    Establish a connection to the SQLite database.
+    Returns a connection object.
+    """
+    connection = sqlite3.connect('instance/database.db')  # Replace with your database file name
+    connection.row_factory = sqlite3.Row  # To access rows like dictionaries
+    return connection
 
 @weather_bp.route('/weather-data')
 def weather_data():
-    # Query all locations first
-    locations_query = text('SELECT DISTINCT location FROM WeatherData ORDER BY location')
-    locations = [loc[0] for loc in db.session.execute(locations_query).fetchall()]
-    
+    # Establish database connection
+    conn = get_db_connection()
+
+    # Query all unique locations
+    locations_query = 'SELECT DISTINCT location FROM WeatherData ORDER BY location'
+    locations = [row['location'] for row in conn.execute(locations_query).fetchall()]
+
     # Get selected location from query parameter
     selected_location = request.args.get('location', 'all')
-    
+
     # Build query based on location filter
     if selected_location and selected_location != 'all':
-        query = text('SELECT * FROM WeatherData WHERE location = :location ORDER BY timestamp')
-        weather_records = db.session.execute(query, {'location': selected_location}).fetchall()
+        query = 'SELECT * FROM WeatherData WHERE location = ? ORDER BY timestamp'
+        weather_records = conn.execute(query, (selected_location,)).fetchall()
     else:
-        query = text('SELECT * FROM WeatherData ORDER BY timestamp')
-        weather_records = db.session.execute(query).fetchall()
+        query = 'SELECT * FROM WeatherData ORDER BY timestamp'
+        weather_records = conn.execute(query).fetchall()
 
+    # Close the database connection
+    conn.close()
+
+    # Render the template with data
     return render_template('weather-data.html', 
-                         weather_records=weather_records, 
-                         locations=locations,
-                         selected_location=selected_location)
+                           weather_records=weather_records, 
+                           locations=locations,
+                           selected_location=selected_location)
